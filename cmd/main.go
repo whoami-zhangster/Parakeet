7406ce49ab3fb7c0f0aa08f51e7c7b01b8e27de7
whoami-zhangster <Aaron.Zhang1995@gmail.com>
2020-04-25 19:03:43 -0400

    :construction: logger, multi-endpoint creation

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"time"
+
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"go.uber.org/zap"
 
 	conf "github.com/whoami-zhangster/Parakeet/pkg/config"
 )
 
 func main() {
+	// create logger
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+	log := logger.Sugar()
+	log.Info("created logger")
+
 	// Config file from command line args as byte array
-	endpointConfig, err := conf.Setup()
+	endpointConfig, err := conf.Setup(log)
 	if err != nil {
-		panic("TODO ")
+		log.Fatal(err)
+		panic(err)
 	}
 
-	fmt.Printf("\n%+v\n", *endpointConfig)
-
 	// create endpoint w/ operations and payload
+	r := mux.NewRouter()
+	r.HandleFunc(endpointConfig.Path, endpointConfig.CreateHandleFunc())
+
+	srv := &http.Server{
+		Addr: "0.0.0.0:8080",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
--- pkg/config/parse.go
+++ pkg/config/parse.go
@@ -5,19 +5,22 @@ import (
 	"flag"
 	"io/ioutil"
 
+	"github.com/whoami-zhangster/Parakeet/pkg/logger"
 	"github.com/whoami-zhangster/Parakeet/pkg/rest"
 
 	"github.com/go-yaml/yaml"
 )
 
 // Setup takes the configFile path as a command line argument, loads the file and parses it into Endpoint struct
-func Setup() (*rest.Endpoint, error) {
+func Setup(log logger.Logger) (*rest.Endpoint, error) {
 	var configFile string
 	flag.StringVar(&configFile, "config", "config.yaml", "config filepath for yaml file")
 	flag.Parse()
 	if configFile == "" {
 		return nil, errors.New("no config file provided")
 	}
+
+	log.Debugf("reading file %s", configFile)
 
 	cf, err := ioutil.ReadFile(configFile)
 	if err != nil {
